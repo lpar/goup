78bf2ebc82188bb992d7faebc870b5e1e72d7655
Andrew Field <andrewpcfield@gmail.com>
2019-02-16 21:14:29 +0200

    Doesn't need else statement.

--- main.go
+++ main.go
@@ -278,9 +278,8 @@ func downloadAndInstall(dl *GoDownload) error {
 	tmpfile, shasum, err := downloadFile(dl)
 	if err != nil {
 		return fmt.Errorf("tmpfile download failed: %v", err)
-	} else {
-		fmt.Printf("tmpfile downloaded successfully into %v\n", tmpfile)
 	}
+	fmt.Printf("tmpfile downloaded successfully into %v\n", tmpfile)
 	if shasum != dl.SHA256 {
 		return fmt.Errorf("bad checksum, expected %s got %s", dl.SHA256, shasum)
 	}
